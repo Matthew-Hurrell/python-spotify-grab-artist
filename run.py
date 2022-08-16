8ebdfd073d7e684a7a4c196db6c0bdf7260de1dd
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-16 10:40:57 +0000

    Add while loop to create ordered list. Change artist to Foo Fighters

--- run.py
+++ run.py
@@ -7,14 +7,17 @@ SPOTIPY_CLIENT_SECRET = "d41061364f1b40c681bdf30d5b87d8c8"
 SPOTIPY_REDIRECT_URI = "https://localhost:8000/callback"
 """
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+uri = 'spotify:artist:7jy3rLJdDQY21OgRLCZ9sD'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
+results = spotify.artist_albums(uri, album_type='album')
 albums = results['items']
 while results['next']:
     results = spotify.next(results)
     albums.extend(results['items'])
 
-for album in albums:
-    print(album['name'])
+i = 1
+while(i < len(albums)):
+    for album in albums:
+        print(f"{i}. {album['name']}")
+        i += 1
