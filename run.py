f56db7c81f6384ebb996724b2d090f4e5ee60a25
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-16 10:07:04 +0000

    Add enviroment id, secret and URI with basic functionality

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+"""
+SPOTIPY_CLIENT_ID = "7ba153190c884278957b14d76473e624"
+SPOTIPY_CLIENT_SECRET = "d41061364f1b40c681bdf30d5b87d8c8"
+SPOTIPY_REDIRECT_URI = "https://localhost:8000/callback"
+"""
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
